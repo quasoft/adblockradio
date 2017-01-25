0f4cb15a0bfe2b35272c1af61d28137b84188cb7
QuaSoft <info@quasoft.net>
2017-01-25 22:20:50 +0200

    Add 'Search for lyrics' context menu in favourites list

--- ui/dlg_favourites_editor.py
+++ ui/dlg_favourites_editor.py
@@ -1,3 +1,8 @@
+from PyQt4.QtCore import Qt
+from PyQt4.QtGui import QAction
+from PyQt4.QtCore import SIGNAL
+
+import utils
 from .dlg_text_item_editor import DlgTextItemEditor
 
 
@@ -7,3 +12,14 @@ class DlgFavouritesEditor(DlgTextItemEditor):
         self.itemName = "Favourite"
 
         self.setWindowTitle("Favourites editor")
+
+        self.list_view.setContextMenuPolicy(Qt.ActionsContextMenu)
+
+        action_search_for_lyrics = QAction("Search for lyrics online", self)
+        action_search_for_lyrics.triggered.connect(self.on_search_for_lyrics)
+        self.list_view.addAction(action_search_for_lyrics)
+
+    def on_search_for_lyrics(self):
+        title, index = self.get_selected()
+        if title:
+            utils.open_in_azlyrics(title)
